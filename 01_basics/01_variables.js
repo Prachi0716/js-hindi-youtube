1e0c5d68751373331fd185ed48ea302159813b56
Prachi0716 <pd191097@gmail.com>
2025-02-14 12:34:31 +0530

    Added new files and folders

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let  accountEmail = "pdcvdj"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+//accountId =2
+accountEmail ="ghgabnd"
+accountPassword = "68545"
+console.log(accountId);
+console.log(accountPassword);
+console.table([accountEmail,accountId,accountPassword,accountCity]);
+
